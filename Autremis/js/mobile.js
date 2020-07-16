f3f15aed3706973d3a262cf74f153c40a9ee1d3e
Kipsazz <kbickus@gmail.com>
2020-07-16 18:04:29 +0300

    mobile menu js fixes

--- Autremis/js/mobile.js
+++ Autremis/js/mobile.js
@@ -29,11 +29,9 @@ window.onload = function(){
 		var b = sub.getAttribute('style');
 		if(b){
 			sub.removeAttribute('style');
-			getElm[i].lastChild.style.backgroundImage='url(images/mobile-expand.png)';
 			getElm[i].lastChild.style.backgroundColor='rgba(11, 163, 156, 0.7)';
 		} else {
 			sub.style.display='block';
-			getElm[i].lastChild.style.backgroundImage='url(images/mobile-collapse.png)';
 			getElm[i].lastChild.style.backgroundColor='rgba(0,0,0,0.8)';
 		}
 	};
