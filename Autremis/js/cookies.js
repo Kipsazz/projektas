5f327d510f1def88e9aea904ca4d1c57eeb829fe
Kipsazz <kbickus@gmail.com>
2020-07-16 16:59:15 +0300

    kometarai css

--- Autremis/js/cookies.js
+++ Autremis/js/cookies.js
@@ -1,14 +1,14 @@
-/* TODO fix the storage and add exp. data https://www.w3schools.com/js/tryit.asp?filename=tryjs_cookie_username */
+/* TODO add exp. data name  https://www.w3schools.com/js/tryit.asp?filename=tryjs_cookie_username */
 const cookieContainer = document.querySelector(".cookie-container");
 const cookieButton = document.querySelector(".cookie-btn");
 
 cookieButton.addEventListener("click", () => {
 	cookieContainer.classList.remove("active");
-	localStorage.setItem("AutremisCookieBannerDisplayed", "true");
+	document.cookie = ("AutremisCookieBannerDisplayed", "true");
 });
 
 setTimeout(() => {
-	if (!localStorage.getItem("AutremisCookieBannerDisplayed")) {
+	if (document.cookie < ("AutremisCookieBannerDisplayed")) {
 		cookieContainer.classList.add("active");
 		}
 }, 2000);
